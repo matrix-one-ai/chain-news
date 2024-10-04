1245196b627b16613df94ab0bcd7e93a76211590
Christopher Trimboli <cjft@trimboli.io>
2024-10-03 22:17:31 -0600

    fix indexedDB SSR

--- app/helpers/indexedDb.ts
+++ app/helpers/indexedDb.ts
@@ -1,19 +1,28 @@
-import { openDB } from "idb";
+"use client";
+
+import { IDBPDatabase, openDB } from "idb";
 
 const DB_NAME = "VRMCacheDB";
 const STORE_NAME = "vrmFiles";
 
-export const dbPromise = openDB(DB_NAME, 1, {
-  upgrade(db) {
-    if (!db.objectStoreNames.contains(STORE_NAME)) {
-      db.createObjectStore(STORE_NAME);
-    }
-  },
-});
+let dbPromise: Promise<IDBPDatabase> | undefined;
+
+if (typeof window !== "undefined") {
+  dbPromise = openDB(DB_NAME, 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME);
+      }
+    },
+  });
+}
 
 export const getCachedVRM = async (
   key: string
 ): Promise<ArrayBuffer | undefined> => {
+  if (!dbPromise) {
+    throw new Error("IndexedDB is not available");
+  }
   const db = await dbPromise;
   return db.get(STORE_NAME, key);
 };
@@ -22,6 +31,9 @@ export const cacheVRM = async (
   key: string,
   data: ArrayBuffer
 ): Promise<void> => {
+  if (!dbPromise) {
+    throw new Error("IndexedDB is not available");
+  }
   const db = await dbPromise;
   await db.put(STORE_NAME, data, key);
 };
