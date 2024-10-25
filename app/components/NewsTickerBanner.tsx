c97c2062aa077b5102d45a4f4ff7dfa29d59ef98
Christopher Trimboli <cjft@trimboli.io>
2024-10-24 22:26:20 -0600

    add custom logo to ticker banner

--- app/components/NewsTickerBanner.tsx
+++ app/components/NewsTickerBanner.tsx
@@ -3,6 +3,7 @@ import { News } from "@prisma/client";
 import React from "react";
 import Marquee from "react-fast-marquee";
 import { load } from "cheerio";
+import Image from "next/image";
 
 interface NewTickerBannerProps {
   newsItems: News[];
@@ -26,9 +27,24 @@ const NewsTickerBanner = ({ newsItems }: NewTickerBannerProps) => (
         sx={{
           letterSpacing: "0.1rem",
           textTransform: "uppercase",
+          display: "flex",
+          alignItems: "center",
         }}
       >
-        {newsItems.map((newsItem) => parseHTML(newsItem.title)).join(" • ")}
+        {newsItems.map((newsItem, index) => (
+          <React.Fragment key={newsItem.id}>
+            {parseHTML(newsItem.title)}
+            {index < newsItems.length - 1 && (
+              <Image
+                src="/images/Master-Emblem.svg"
+                alt="separator"
+                width={50}
+                height={20}
+                style={{ margin: "0 2rem" }}
+              />
+            )}
+          </React.Fragment>
+        ))}
       </Typography>
     </Marquee>
   </Box>
