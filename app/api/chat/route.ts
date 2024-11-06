93d1ed4fe3c48e06a4b2ce8c86b85a0b0b1bacfd
Christopher Trimboli <cjft@trimboli.io>
2024-11-05 22:49:13 -0700

    add tokenizer length check to API

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
 import { CoreMessage, streamText } from "ai";
 import { createAzure } from "@ai-sdk/azure";
+import { NextResponse } from "next/server";
+import { isWithinTokenLimit } from "gpt-tokenizer";
 
 const azure = createAzure({
   resourceName: process.env.AZURE_OPENAI_RESOURCE!,
@@ -7,13 +9,26 @@ const azure = createAzure({
 });
 
 export async function POST(req: Request) {
-  const { messages }: { messages: CoreMessage[] } = await req.json();
+  try {
+    let { messages }: { messages: CoreMessage[] } = await req.json();
 
-  const result = await streamText({
-    model: azure("gpt-4o"),
-    system: "You are a helpful assistant.",
-    messages,
-  });
+    const messagesText = messages.map((message) => message.content).join("");
 
-  return result.toDataStreamResponse();
+    const withinTokenLimit = isWithinTokenLimit(messagesText, 125000);
+
+    // if the messages exceed the token limit, only send the most recent 3 messages
+    if (!withinTokenLimit) {
+      messages = messages.slice(-3);
+    }
+
+    const stream = await streamText({
+      model: azure("gpt-4o"),
+      system: "You are a helpful assistant.",
+      messages,
+    });
+
+    return stream.toDataStreamResponse();
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
