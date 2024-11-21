e7edbb4a1f809f2793b4c6a4209b703ee70c29a4
Christopher Trimboli <cjft@trimboli.io>
2024-11-21 10:44:43 -0700

    fix helio id mapping

--- app/helpers/helio.ts
+++ app/helpers/helio.ts
@@ -8,12 +8,12 @@ export enum PayTypes {
 export const getPayTypePaylink = (payType: PayTypes) => {
   switch (payType) {
     case PayTypes.SMALL:
-      return process.env.NEXT_PUBLIC_HELIO_SMALL_PAYLINK_ID!;
+      return process.env.NEXT_PUBLIC_HELIO_SMALL_PAYLINK!;
     case PayTypes.LARGE:
-      return process.env.NEXT_PUBLIC_HELIO_LARGE_PAYLINK_ID!;
+      return process.env.NEXT_PUBLIC_HELIO_LARGE_PAYLINK!;
     case PayTypes.SMALL_MATRIX:
-      return process.env.NEXT_PUBLIC_HELIO_SMALL_MATRIX_PAYLINK_ID!;
+      return process.env.NEXT_PUBLIC_HELIO_SMALL_MATRIX_PAYLINK!;
     case PayTypes.LARGE_MATRIX:
-      return process.env.NEXT_PUBLIC_HELIO_LARGE_MATRIX_PAYLINK_ID!;
+      return process.env.NEXT_PUBLIC_HELIO_LARGE_MATRIX_PAYLINK!;
   }
 };
