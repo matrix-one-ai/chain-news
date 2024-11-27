2a47f1394962ca7354af47aaa867a8adf2de66c9
Christopher Trimboli <cjft@trimboli.io>
2024-11-27 10:17:44 -0700

    vanilla gtags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import theme from "./theme";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Head from "next/head";
 
 const ibmPlexSans = IBM_Plex_Sans({
   weight: ["400", "500", "600"],
@@ -64,6 +65,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Existing head content */}
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl';f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-553DSNX9');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
       <body
         className={`${ibmPlexSans.variable} ${ibmPlexMono.variable} ${pixelifySans.variable}`}
       >
@@ -78,6 +94,14 @@ export default function RootLayout({
         </AppRouterCacheProvider>
         <Analytics />
         <CssBaseline />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-553DSNX9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
       <GoogleAnalytics gaId="GTM-553DSNX9" />
     </html>
