c72cfb47250aee175793d239cfe8fda00dcd591d
Christopher Trimboli <cjft@trimboli.io>
2024-12-06 10:25:48 -0700

    upgrade gpt again

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: Request) {
     }
 
     const stream = await streamText({
-      model: azure("gpt-4o-mini"),
+      model: azure("gpt-4o"),
       messages: localMessages,
       frequencyPenalty: 0.5,
       presencePenalty: 0.5,
